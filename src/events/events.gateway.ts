ffd989f497985050153993df871c31a7b97ffc2f
M4cr0s3 <tmmllllllllll@gmail.com>
2024-04-16 22:51:50 +0300

    updated: ws mock

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -6,17 +6,57 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { OnModuleInit } from '@nestjs/common';
 
-@WebSocketGateway({ namespace: '/events' })
-export class EventsGateway {
+@WebSocketGateway({ namespace: '/test' })
+export class EventsGateway implements OnModuleInit {
   @WebSocketServer()
   server: Server;
+  private code = '1234';
+  private clientId: string = 'dAF7SAnjAS_';
+  private roomId: string = 'lutaya-komnata-dlya-testa';
 
-  @SubscribeMessage('message')
-  onMessage(
+  @SubscribeMessage('game:start')
+  onStartGame(
     @ConnectedSocket() client: Socket,
-    @MessageBody() payload: any,
+    @MessageBody() data: { clientId: string },
   ): any {
-    this.server.emit('onMessage', { client: client.id, message: payload });
+    if (data.clientId !== this.clientId) {
+      return;
+    }
+
+    this.server
+      .to(this.roomId)
+      .emit('game:start', { msg: 'Да начнется рубилово...' });
+  }
+
+  @SubscribeMessage('user:registerTeam')
+  onRegister(
+    @ConnectedSocket() client: Socket,
+    @MessageBody()
+    data: {
+      clientId: string;
+      teamName: string;
+      code: string;
+    },
+  ): any {
+    const { clientId, teamName, code } = data;
+    this.server.emit('user:registerTeam', { clientId, teamName, code });
+  }
+
+  @SubscribeMessage('user:verifyCode')
+  onVerifyCode(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: { code: string; clientId: string },
+  ): any {
+    const { code, clientId } = data;
+    client.emit('user:verifyCode', { success: code === this.code });
+  }
+
+  onModuleInit(): any {
+    this.server.on('connect', (client: Socket) => {
+      client.join(this.roomId);
+      console.log(client.rooms);
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import Redis from 'ioredis';
+
+export const redis = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: +process.env.REDIS_PORT || 6379,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+
+  await app.listen(8080);
 }
 
 bootstrap();
